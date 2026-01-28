d6bbecc44b82b6de1fc2b7b979213a035ad55475
Andy681995 <alex.nike6895@gmail.com>
2026-01-28 16:29:58 +0300

    Fix cloudinary config

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,37 +1,30 @@
-const path = require('path');
-// Парсим строку подключения postgres://user:pass@host:port/db
-const parse = require('pg-connection-string').parse;
+import path from 'path';
 
-module.exports = ({ env }) => {
-  // Если есть переменная DATABASE_URL (мы её добавим в облаке), используем Postgres
-  if (env('DATABASE_URL')) {
-    const config = parse(env('DATABASE_URL'));
-    return {
+export default ({ env }) => {
+  // Если есть ссылка на облачную базу — включаем Postgres. Если нет — старый добрый SQLite.
+  const client = env('DATABASE_URL') ? 'postgres' : 'sqlite';
+
+  const connections = {
+    postgres: {
       connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false, // Важно для Neon/Render
-          },
-        },
-        debug: false,
+        connectionString: env('DATABASE_URL'),
+        ssl: { rejectUnauthorized: false }, // Нужно для Neon/Render
       },
-    };
-  }
-
-  // Иначе (на компьютере) оставляем SQLite
-  return {
-    connection: {
-      client: 'sqlite',
+      pool: { min: 2, max: 10 },
+    },
+    sqlite: {
       connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: path.join(__dirname, '..', '..', '.tmp/data.db'),
       },
       useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
   };
 };
